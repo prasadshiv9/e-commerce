d4255e9d5e0d690ceb376cff33283dc7608cb888
prasadshiv9 <pshivraaz9@gmail.com>
2022-11-30 21:36:42 +0530

    redux

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,21 @@
+import { ActionTypes } from "../contants/action-types";
+
+const initialState = {
+    products: [{
+        id:1,
+        title:"Shiv",
+        category: "Programmer"
+    }],
+};
+
+export const productReducer = (state, {type, payload})=>{
+        switch(type){
+            case ActionTypes.SET_PRODUCTS:
+                return state;
+
+                default:
+                    return state;
+
+        }
+
+}
